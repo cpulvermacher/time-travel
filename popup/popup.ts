46bde1492bb6fbefa420faaacb024194e24ae17d
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2023-03-18 22:10:31 +0900

    use typescript

--- popup/popup.ts
+++ popup/popup.ts
@@ -1,65 +1,65 @@
 async function getActiveTabId() {
-    let queryOptions = { active: true, currentWindow: true };
+    let queryOptions = { active: true, currentWindow: true }
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
-    let [tab] = await chrome.tabs.query(queryOptions);
-    return tab.id;
+    let [tab] = await chrome.tabs.query(queryOptions)
+    return tab.id
 }
 
 //Note: needs 'storage' permission
-async function storageGet(key) {
-    return (await chrome.storage.local.get([key]))[key];
+async function storageGet(key: string) {
+    return (await chrome.storage.local.get([key]))[key]
 }
 
-function setFakeDate(date) {
-    window.TT_FAKE_DATE = date || undefined;
+function setFakeDate(date: string) {
+    window.TT_FAKE_DATE = date || undefined
 }
 
-async function injectFakeDate(fakeDate) {
+async function injectFakeDate(fakeDate: string) {
     console.log('injecting fake date', fakeDate)
-    const tabId = await getActiveTabId();
+    const tabId = await getActiveTabId()
 
     await chrome.scripting.executeScript({
         target: { tabId: tabId },
         func: setFakeDate,
         args: [fakeDate],
         world: 'MAIN'
-    });
+    })
 }
 
-async function onFakeDate(fakeDate) {
-    await injectFakeDate(fakeDate);
-    await chrome.storage.local.set({ fakeDate });
+async function onFakeDate(fakeDate: string) {
+    await injectFakeDate(fakeDate)
+    await chrome.storage.local.set({ fakeDate })
 
     //TODO error handling (we might not get a tab)
     await chrome.action.setBadgeText({
         tabId: await getActiveTabId(),
         text: fakeDate ? 'ON' : ''
-    });
-    window.close();
+    })
+    window.close()
 }
 
-const input = document.getElementById('fakeDateInput');
+const input: HTMLInputElement = document.getElementById('fakeDateInput')
 
 const fakeDateFromStorage = await storageGet('fakeDate')
-const initialValue = fakeDateFromStorage || (new Date()).toISOString();
-input.setAttribute('value', initialValue);
+const initialValue = fakeDateFromStorage || (new Date()).toISOString()
+input.setAttribute('value', initialValue)
 
 
 document.getElementById('setBtn').onclick = async () => {
-    const fakeDate = input.value;
+    const fakeDate = input.value
     //TODO validate
     await onFakeDate(fakeDate)
-};
+}
 
 input.onkeydown = async (event) => {
     if (event.key == 'Enter') {
-        event.preventDefault();
+        event.preventDefault()
 
-        const fakeDate = input.value;
+        const fakeDate = input.value
         //TODO validate
         await onFakeDate(fakeDate)
     }
-};
+}
 
 document.getElementById('resetBtn').onclick = async () => {
     await onFakeDate('')
