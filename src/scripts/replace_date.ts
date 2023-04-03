3c9d7429408da9028a5796a46bbe2c4f61aed60b
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2023-04-03 17:11:50 +0900

    make injected script an IIFE to avoid conflicts

--- src/scripts/replace_date.ts
+++ src/scripts/replace_date.ts
@@ -1,45 +1,47 @@
-function getFakeDate(): string | null {
-    const FAKE_DATE_STORAGE_KEY = 'timeTravelDate'
-    try {
-        return window.sessionStorage.getItem(FAKE_DATE_STORAGE_KEY)
-    } catch (err) {
-        //in sandbox, we might not be able to access sessionStorage
-        return null
+(() => {
+    function getFakeDate(): string | null {
+        const FAKE_DATE_STORAGE_KEY = 'timeTravelDate'
+        try {
+            return window.sessionStorage.getItem(FAKE_DATE_STORAGE_KEY)
+        } catch (err) {
+            //in sandbox, we might not be able to access sessionStorage
+            return null
+        }
     }
-}
 
-// needed for constructor
-class FakeDate extends Date {
-    constructor(...options: []) {
-        if (options.length) {
-            super(...options)
-        } else {
-            const fakeDate = getFakeDate()
-            if (fakeDate !== null) {
-                super(fakeDate)
+    // needed for constructor
+    class FakeDate extends Date {
+        constructor(...options: []) {
+            if (options.length) {
+                super(...options)
             } else {
-                super()
+                const fakeDate = getFakeDate()
+                if (fakeDate !== null) {
+                    super(fakeDate)
+                } else {
+                    super()
+                }
             }
         }
     }
-}
 
-// needed for now()
-const originalDateNow = Date.now
-const fakeNow = () => {
-    const fakeDate = getFakeDate()
-    if (fakeDate !== null) {
-        return Date.parse(fakeDate)
-    } else {
-        return originalDateNow()
+    // needed for now()
+    const originalDateNow = Date.now
+    const fakeNow = () => {
+        const fakeDate = getFakeDate()
+        if (fakeDate !== null) {
+            return Date.parse(fakeDate)
+        } else {
+            return originalDateNow()
+        }
     }
-}
 
-// FakeDate does not support all of Date's features right now, replace only when we already have a fake date set
-// this means on you need to reload the page at least once after setting a date, but it's better than breaking
-// random web pages
-if (getFakeDate() != null) {
-    // eslint-disable-next-line no-global-assign
-    Date = FakeDate as DateConstructor
-    Date.now = fakeNow
-}
+    // FakeDate does not support all of Date's features right now, replace only when we already have a fake date set
+    // this means on you need to reload the page at least once after setting a date, but it's better than breaking
+    // random web pages
+    if (getFakeDate() != null) {
+        // eslint-disable-next-line no-global-assign
+        Date = FakeDate as DateConstructor
+        Date.now = fakeNow
+    }
+})()
