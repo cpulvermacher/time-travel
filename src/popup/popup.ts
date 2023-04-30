c0efcc1e65353442534b9d294872b58aae442c7f
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2023-04-30 14:19:38 +0900

    avoid persistent content scripts over sessions

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -36,7 +36,8 @@ async function registerContentScriptIfNeeded(tabId: number | undefined) {
         ],
         'runAt': 'document_start',
         'world': 'MAIN',
-        'allFrames': true
+        'allFrames': true,
+        'persistAcrossSessions': false,
     }, {
         'id': 'sendActive',
         'js': [
@@ -47,7 +48,8 @@ async function registerContentScriptIfNeeded(tabId: number | undefined) {
         ],
         'runAt': 'document_start',
         'world': 'ISOLATED',
-        'allFrames': true
+        'allFrames': true,
+        'persistAcrossSessions': false,
     }]
     const scripts = await chrome.scripting.getRegisteredContentScripts({ ids: contentScripts.map(script => script.id) })
     if (scripts.length > 0) {
