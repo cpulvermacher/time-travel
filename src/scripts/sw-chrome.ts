5dad1b9414684488f4085c6a408ed88f080d7302
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2023-07-12 01:26:55 +0900

    avoid unnecessary await

--- src/scripts/sw-chrome.ts
+++ src/scripts/sw-chrome.ts
@@ -1,17 +1,17 @@
 import { getContentScriptState, setBadgeAndTitle } from '../util/common'
 
-chrome.tabs.onActivated.addListener(async (activeInfo) => {
-    await updateBadgeAndTitle(activeInfo.tabId)
+chrome.tabs.onActivated.addListener((activeInfo) => {
+    updateBadgeAndTitle(activeInfo.tabId)
 })
 
-chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
     if (changeInfo.url == undefined)
         return // url unchanged, nothing to do
 
-    await updateBadgeAndTitle(tabId)
+    updateBadgeAndTitle(tabId)
 })
 
-chrome.runtime.onMessage.addListener(async (message, sender) => {
+chrome.runtime.onMessage.addListener((message, sender) => {
     if (message.msg == 'active' && sender.tab?.id) {
         const state = {
             isScriptInjected: true,
@@ -19,7 +19,7 @@ chrome.runtime.onMessage.addListener(async (message, sender) => {
             clockIsRunning: message.isClockTicking,
             fakeDateActive: true
         }
-        await setBadgeAndTitle(sender.tab.id, state)
+        setBadgeAndTitle(sender.tab.id, state)
     }
 })
 
