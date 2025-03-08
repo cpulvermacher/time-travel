faf970102ffaa32b034fd0f62bfe6b0109422a73
Christian Pulvermacher <christian.pulvermacher@posteo.de>
2025-03-08 16:34:37 +0900

    simplify date format info text

--- src/popup/DateFormatInfo.svelte
+++ src/popup/DateFormatInfo.svelte
@@ -8,36 +8,27 @@
 </script>
 
 <Modal {onClose} closeOnCancel={true}>
-    Supported formats:
-    <dl>
-        <dt>2023-04-27 12:40</dt>
-        <dd>Local time</dd>
-        <dt>2023-04-27</dt>
-        <dd>Midnight UTC</dd>
-        <dt>2025-03-30 00:59:55</dt>
-        <dd>5 seconds before a one-hour jump to 2 a.m. (summer time) in Europe/London</dd>
-        <dt>2023-04-27T12:40Z</dt>
-        <dd>Set local equivalent for given UTC time</dd>
-        <dt>2023-03-25T12:40:00.120</dt>
-        <dd>Local time with milliseconds</dd>
-        <dt>1731493140025</dt>
-        <dd>UNIX timestamp</dd>
-    </dl>
+    <p>
+        You can enter date and time in any format supported by <code>Date.parse()</code> as well as UNIX timestamps:
+    </p>
+    <ul>
+        <li>2025-02-27 12:40</li>
+        <li>2025-02-27 (midnight)</li>
+        <li>2025-03-30 00:59:55</li>
+        <li>2025-04-27T12:40Z</li>
+        <li>2025-02-25T12:40:00.120</li>
+        <li>1731493140025</li>
+    </ul>
 </Modal>
 
 <style>
-    dl {
-        margin: 0;
-        padding: 0;
+    p,
+    ul {
+        width: 100%;
     }
-    dt {
-        font-weight: bold;
+    li {
+        list-style: none;
         margin: 0;
-        margin-inline-start: 0;
-    }
-    dd {
-        padding: 0;
-        margin: 0;
-        margin-inline-start: 10px;
+        margin-inline-start: 20px;
     }
 </style>
