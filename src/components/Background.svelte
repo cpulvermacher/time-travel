cpulvermacher/time-travel

commit 2211e0d715da230f14ca9c80614d169b3c642bef

explictly untrack some props

src/components/Background.svelte
@@ -5,7 +5,8 @@
     interface Props {
         effectiveDate: Date | undefined;
     }
-    const { effectiveDate }: Props = $props();
+    const props: Props = $props();
+    const effectiveDate = untrack(() => props.effectiveDate);
 
     let lastEffectiveDate = $state(effectiveDate);
     let numSpins: number | undefined = $state();

src/components/Settings.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+    import { untrack } from 'svelte';
     import { m } from '../paraglide/messages';
     import { reloadTab, withTabLoadingRetry } from '../util/browser';
     import { disableFakeDate, setClockState, setFakeDate } from '../util/content-script-state';
     import { parseDate } from '../util/date-utils';
     import { updateExtensionIcon } from '../util/icon';
-    import { saveMostRecentTimezone, saveSetting, type Settings } from '../util/settings';
+    import { saveMostRecentTimezone, saveSetting } from '../util/settings';
     import Accordion from './Accordion.svelte';
     import Background from './Background.svelte';
     import DateTimePicker from './DateTimePicker.svelte';
@@ -12,13 +13,13 @@
     import ReloadModal from './ReloadModal.svelte';
     import TimezoneSelect from './TimezoneSelect.svelte';
     import Toggle from './Toggle.svelte';
+    import type { InitialState } from '../popup/initial-state';
 
     interface Props {
-        isEnabled: boolean;
-        fakeDate: string;
-        settings: Settings;
+        initialState: InitialState;
     }
-    const initialState: Props = $props();
+    const props: Props = $props();
+    const initialState = untrack(() => props.initialState);
 
     let errorMsg = $state<string>();
     let showReloadModal = $state(false);

src/popup/Popup.svelte
@@ -18,7 +18,7 @@
         <div class="loading-spinner"></div>
     </div>
 {:then initialState}
-    <Settings fakeDate={initialState.fakeDate} isEnabled={!!initialState.isEnabled} settings={initialState.settings} />
+    <Settings {initialState} />
 {:catch error}
     <div class="error">
         <p>{error instanceof Error ? error.message : ''}</p>

src/popup/initial-state.ts
@@ -5,7 +5,7 @@ import { getContentScriptState } from '../util/content-script-state';
 import { formatLocalDate, parseDate, parseTimestamp } from '../util/date-utils';
 import { loadSettings, type Settings } from '../util/settings';
 
-type InitialState = {
+export type InitialState = {
     isEnabled: boolean;
     fakeDate: string; // current fake date, or current time as fallback
     settings: Settings; // stored settings, but possibly overridden by tab state if active

commit 01e4bfa7f9f2279ab3bc82b3671340841128c1e6

fix background animation
regression in 2211e0d

src/components/Background.svelte
@@ -6,9 +6,9 @@
         effectiveDate: Date | undefined;
     }
     const props: Props = $props();
-    const effectiveDate = untrack(() => props.effectiveDate);
+    const effectiveDate = $derived(props.effectiveDate);
 
-    let lastEffectiveDate = $state(effectiveDate);
+    let lastEffectiveDate = $state(untrack(() => effectiveDate));
     let numSpins: number | undefined = $state();
     let desaturated = $derived(effectiveDate === undefined);
 